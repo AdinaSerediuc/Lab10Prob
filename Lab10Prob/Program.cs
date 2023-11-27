418a4c8e31302d21cdaf50a95ee3fa1658b706ea
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-11-27 21:39:04 +0200

    Add project files.

---
+++ Lab10Prob/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Exercitiu 1
+ * Un student este caracterizat de
+• Id (unic)
+• Nume
+• Prenume
+• Varsta
+• Specializare
+
+• Specializarile vor fi:
+• Informatica
+• Litere
+• Constructii
+
+• Creeati modelul, adaugati EF 3.1,
+Adaugati DB
+• Creeati in clasa “main” o functie
+“Seed” care va popula DB-ul
+• Afisati toti studentii din DB in
+ordine alfabetica
+• Afisati cel mai tanar student de
+la constructii cu varsta de peste
+20 de ani*/
+
+namespace Lab10Prob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
